8bc40321b1a31fe0f477008b07d13558e0291df3
Anu790j <91656728+Anu790j@users.noreply.github.com>
2023-03-17 00:38:46 +0530

    Duplicate Elements in Array leetcode ques

---
+++ Arrays/Find_All_Duplicates.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int Duplicates(int arr[ ], int size)
+{
+   
+}
+
+int main()
+{
+    int arr[ ]={4,3,2,7,8,2,3,1};
+
+    Duplicates(arr,8);
+    return 0;
+}
