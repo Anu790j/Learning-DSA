0b4850abd343098da874fedcb4ab4bd64e4ffbbc
Anu790j <91656728+Anu790j@users.noreply.github.com>
2023-04-01 12:24:10 +0530

    Find pivot element in an array

---
+++ Arrays/Pivot_ele_rotated_arr.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int findPivot(int arr[], int size)
+{
+    int s = 0;
+    int e = size-1;
+    int mid = s + (e - s)/2;
+
+    while(s<e){
+
+        if(arr[mid] >= arr[0] )     // It means we are on first line.
+        {
+            s =mid+1;               //To move closer to the pivot element.
+        }
+        else{
+            e=mid;                  // Mid itself can be pivot and we are on second line.
+        }
+
+        mid = s + (e - s)/2;
+
+    }
+    return s;
+}
+
+int main(){
+
+    int rot_arr[]= {5,6,7,1,2,3,4};
+
+    int pivot= findPivot(rot_arr,7);
+
+    cout<<"Pivot element found at "<<pivot;
+
+    return 0;
+}
